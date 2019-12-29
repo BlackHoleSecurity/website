f53109ddba0849528d9a588c15ca67fd14934ff3
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-30 03:49:51 +0700

    renamed

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://9e2c5fe5.ngrok.io/mblog';
+$config['base_url'] = 'http://localhost:8080';
 
 /*
 |--------------------------------------------------------------------------
