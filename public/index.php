9f5ba0ee3fec10f3f92f5f044293e55bd3307008
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-12-31 23:46:16 +0700

    error handling

--- public/index.php
+++ public/index.php
@@ -1,39 +1,54 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-$app = new Slim\App(
-    [
-        'settings' => [
-            'displayErrorDetails' => true
-        ]
-    ]
+$container = new \Slim\Container();
+$app = new \Slim\App($container);
+$container = $app->getContainer();
+$container['db'] = function ($container) {
+    return new PDO('mysql:host=localhost;dbname=blog_db', 'root', 'root');
+};
+
+$app->post(
+    '/post',
+    function ($request, $response) {
+        $args = $request->getParsedBody();
+        if (!isset($args['username'])) {
+            $error[] = 'Incorrect Username';
+        }
+
+        if (!isset($args['password'])) {
+            $error[] = 'Incorrect Password';
+        }
+
+        if (isset($error)) {
+            $message = $error;
+            $status = false;
+            $status_code = 417;
+        } else {
+            $message = 'Postingan anda berhasil di publish';
+            $status = true;
+            $status_code = 200;
+        }
+
+        $data = [
+            'status' => $status,
+            'message' => $message
+        ];
+        return $response
+            ->withJson($data, $status_code)
+            ->withHeader('Allow', 'GET, POST');
+    }
 );
 
 $app->get(
-    '/post/{username}/{password}/{title}/{slug}/{content}/{meta}',
-    function ($request, $response, $args) {
-        if(true) {
-            $data = [
-                'status' => true,
-                'message' => 'Postingan anda telah di publish'
-            ];
-            $status_code = 200;
-        }
-        else {
-            $error[]='Incorrect Password';
-            $error[]='Incorrect Username';
-            $error[]='Can\'t Parse content';
-            $error[]='Can\'t Parse slug';
-            $error[]='Can\'t Parse title';
-            $error[]='Can\'t Parse meta';
-
-            $data = [
-                'status' => false,
-                'message' => $error
-            ];
-            $status_code = 417;
-        }
-
-        return $response->withJson($data, $status_code);
+    '/post',
+    function ($request, $response) {
+        $database = $this->get('db');
+        $statement = $database->prepare(
+            'SELECT user_name, user_email FROM tbl_user'
+        );
+        $statement->execute();
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $response->withJson($data)->withHeader('Allow', 'GET, POST');
     }
 );
 
